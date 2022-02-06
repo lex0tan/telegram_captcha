fabca68a9443a998455d16496f1fcfc7ee2ad411
lex0tan <96478989+lex0tan@users.noreply.github.com>
2022-02-06 05:27:02 +0100

    imports.py

---
+++ imports.py
@@ -0,0 +1,7 @@
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon import TelegramClient, events, Button
+from telethon.sync import TelegramClient
+from telethon.client import buttons 
+api_id = useyourown
+api_hash = "useyourown"
+bot_token = "useyourown"
